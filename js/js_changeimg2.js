2d25d81cd64413a5dfa8b11510b13068dcf3a593
ruanjiangongcheng250 <1032094832@qq.com>
2016-07-14 14:27:04 +0800

    add css and js and img

---
+++ js/js_changeimg2.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var speed=10;
+marquee_product2.innerHTML=marquee_product1.innerHTML;
+function Marquee(){
+if(marquee_demo.scrollLeft>=marquee_product1.scrollWidth){
+marquee_demo.scrollLeft=0;
+}
+else{
+marquee_demo.scrollLeft++;
+}
+}
+var MyMar=setInterval(Marquee,speed);
+marquee_demo.onmouseover=function(){clearInterval(MyMar);}
+marquee_demo.onmouseout=function(){MyMar=setInterval(Marquee,speed);}
